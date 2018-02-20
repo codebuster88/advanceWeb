13db2dd99c60639c1277ffeb288567c87e03888a
codebuster88 <31905193+codebuster88@users.noreply.github.com>
2018-02-20 18:34:41 -0500

    knockout lab

---
+++ narnia/narnia/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace narnia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Basic()
+        {
+
+            return View();
+        }
+
+        public ActionResult Advance()
+        {
+            var person = new WebApplication1.Models.Person
+            {
+                FirstName = "Andy",
+                LastName = "Schwab"
+            };
+
+            return View(person);
+        }
+            
+    }
+}
---
+++ narnia/narnia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="narnia.MvcApplication" Language="C#" %>
